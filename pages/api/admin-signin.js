ea572c5aa73ae4dbcc6f28c30ed0ee65ed072da6
tugce <tugce.degentile@gmail.com>
2024-09-21 17:54:06 +0200

    env

--- pages/api/admin-signin.js
+++ pages/api/admin-signin.js
@@ -1,31 +1,33 @@
 // pages/api/admin-signin.js
-import { sign } from 'jsonwebtoken';
-import { serialize } from 'cookie';
+import { sign } from "jsonwebtoken"
+import { serialize } from "cookie"
 
-const SECRET_KEY = process.env.JWT_SECRET;
+const SECRET_KEY = process.env.JWT_SECRET
+const Password = process.env.password
+const Username = process.env.username
 
 export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).end();
+  if (req.method !== "POST") {
+    return res.status(405).end()
   }
 
-  const { username, password } = req.body;
+  const { username, password } = req.body
 
   // In a real application, you would check these credentials against a database
-  if (username === 'admin12' && password === 'koksal1234') {
-    const token = sign({ username }, SECRET_KEY, { expiresIn: '1h' });
-    const serialized = serialize('adminToken', token, {
+  if (username === Username && password === Password) {
+    const token = sign({ username }, SECRET_KEY, { expiresIn: "1h" })
+    const serialized = serialize("adminToken", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
       maxAge: 3600,
-      path: '/',
-    });
-    console.log('serialized:',serialized)
+      path: "/",
+    })
+    console.log("serialized:", serialized)
 
-    res.setHeader('Set-Cookie', serialized);
-    res.status(200).json({ message: 'Success' });
+    res.setHeader("Set-Cookie", serialized)
+    res.status(200).json({ message: "Success" })
   } else {
-    res.status(401).json({ message: 'Invalid credentials' });
+    res.status(401).json({ message: "Invalid credentials" })
   }
 }
